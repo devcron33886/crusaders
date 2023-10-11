c31133f66e96f54150bbe32e8afc1d207250259d
LofiCoders <71966425+devcron33886@users.noreply.github.com>
2023-10-11 14:09:19 +0200

    Added favicon on home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,11 @@
     <meta name="twitter:description" content="{{ $description }}">
     <meta name="twitter:image" content="https://images.pexels.com/photos/1752757/pexels-photo-1752757.jpeg">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.googleapis.com/css2?family=Onest&display=swap" rel="stylesheet">
